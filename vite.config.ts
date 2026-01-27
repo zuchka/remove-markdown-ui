44ef8367275c17f2a0ffc78085d7431b6f2bd495
zuchka <zuchka@users.noreply.github.com>
2026-01-27 15:28:19 -0800

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
     fs: {
-      allow: ["./client", "./shared"],
+      allow: [".", "./client", "./shared"],
       deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
     },
   },
